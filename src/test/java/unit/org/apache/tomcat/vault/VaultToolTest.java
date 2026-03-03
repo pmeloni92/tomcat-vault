cb295e3f264124d4fce672b73b5319d4523cf9a9
pmeloni <pmeloni@redhat.com>
2026-03-03 16:14:49 +0100

    removed security manager from tests and add new exception to avoid some exit commands

--- src/test/java/unit/org/apache/tomcat/vault/VaultToolTest.java
+++ src/test/java/unit/org/apache/tomcat/vault/VaultToolTest.java
@@ -2,9 +2,8 @@ package unit.org.apache.tomcat.vault;
 
 import org.apache.commons.cli.*;
 import org.apache.tomcat.vault.VaultTool;
-import org.junit.After;
+import org.apache.tomcat.vault.exception.VaultException;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -12,59 +11,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
-import java.security.Permission;
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class VaultToolTest {
-
-    SecurityManager sm = System.getSecurityManager();
-
-    //New exception that we need to catch to prevent methods to exit
-    protected static class ExitException extends SecurityException
-    {
-        public final int status;
-        public ExitException(int status)
-        {
-            super(Integer.toString(status));
-            this.status = status;
-        }
-    }
-
-    //New security manager needed to prevent System.exit
-    private static class NoExitSecurityManager extends SecurityManager
-    {
-        @Override
-        public void checkPermission(Permission perm)
-        {
-            // allow anything.
-        }
-        @Override
-        public void checkPermission(Permission perm, Object context)
-        {
-            // allow anything.
-        }
-        @Override
-        public void checkExit(int status)
-        {
-            super.checkExit(status);
-            throw new ExitException(status);
-        }
-    }
-
-    @Before
-    public void setUp()
-    {
-        System.setSecurityManager(new NoExitSecurityManager());
-    }
-
-
-    @After
-    public void tearDown()
-    {
-        System.setSecurityManager(sm);
-    }
-
 
     // Helper method to access private fields using reflection
     private Object getPrivateField(Object instance) throws NoSuchFieldException, IllegalAccessException {
@@ -76,7 +26,7 @@ public class VaultToolTest {
 
     // Test: Parse valid arguments successfully
     @Test
-    public void testValidArguments() throws NoSuchFieldException, IllegalAccessException {
+    public void testValidArguments() throws NoSuchFieldException, IllegalAccessException, VaultException {
         String[] args = {"-k", "keystore.jks", "-p", "password", "-x", "secure-value"};
         VaultTool vaultTool = new VaultTool(args);
 
@@ -99,7 +49,7 @@ public class VaultToolTest {
         String[] args = {"-k", "keystore.jks", "-p"};
         try {
             new VaultTool(args);
-        } catch (ExitException e) {
+        } catch (VaultException e) {
             Assert.assertEquals("2", e.getMessage());
         }
     }
@@ -110,7 +60,7 @@ public class VaultToolTest {
         String[] args = {"-z", "invalid-option"};
         try {
             new VaultTool(args);
-        } catch (RuntimeException e) {
+        } catch (VaultException e) {
             Assert.assertEquals("2", e.getMessage());
         }
     }
@@ -124,14 +74,14 @@ public class VaultToolTest {
         String[] args = {"-k", "keystore.jks", "-p", "password"};
         try {
             new VaultTool(args);
-        } catch (RuntimeException e) {
+        } catch (VaultException e) {
             Assert.assertEquals("2", e.getMessage());
         }
     }
 
     // Test: Help option works without errors
     @Test
-    public void testHelpOption() throws NoSuchFieldException, IllegalAccessException {
+    public void testHelpOption() throws NoSuchFieldException, IllegalAccessException, VaultException {
         String[] args = {"-h"};
         VaultTool vaultTool = new VaultTool(args);
 
